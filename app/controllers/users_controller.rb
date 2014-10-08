d5eb9878388323a8c4f8016d0f491885edf59aa2
Benjamin Loustau <benloustau@gmail.com>
2014-10-08 12:02:11 -0400

    changes to everything. second commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+	before_action 
+
+	def index
+		@user = User.all
+	end
+	
+	def show
+	
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  resources :users do
+    member do
+      post 'follow'
+      post 'unfollow'
+     end
+  end    
+  resources :posts
+  resources :sessions, only: [:new, :create]
+  delete '/logout', to: 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'users#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
